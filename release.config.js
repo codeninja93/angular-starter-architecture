11a7641a67a488940b18643542d361c61381fc30
codeninja93 <deepatel212@gmail.com>
2024-06-29 21:10:29 +0530

    ci: removes the assets

--- release.config.js
+++ release.config.js
@@ -6,7 +6,6 @@ const config = {
 		[
 			'@semantic-release/git',
 			{
-				assets: ['dist/aws/browser/*.*'],
 				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
 			},
 		],
